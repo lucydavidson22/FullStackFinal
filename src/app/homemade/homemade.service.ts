8301c13c26a35d4153157bd11b2d4b23c57fd496
Lucy Davidson <lucy.davidson@outlook.com>
2022-03-30 19:36:59 -0400

    fixing pic styles

--- src/app/homemade/homemade.service.ts
+++ src/app/homemade/homemade.service.ts
@@ -29,11 +29,11 @@ export class HomemadeService {
        (homemades:Homemade[]) => {
          this.homemades = homemades;
          this.maxHomemadeId = this.getMaxId();
-         homemades.sort((a, b) => {
-           if(a.name > b.name){ return 1; }
-           if(a.name < b.name){ return -1; }
-           else { return 0; }
-          });
+        //  homemades.sort((a, b) => {
+        //    if(a.name > b.name){ return 1; }
+        //    if(a.name < b.name){ return -1; }
+        //    else { return 0; }
+        //   });
             let homemadesListClone = this.homemades.slice();
             this.homemadeListChangedEvent.next(homemadesListClone);
        }
